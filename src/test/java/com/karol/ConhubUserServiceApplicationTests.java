99e05f27221fd01d64d08f52d5287bef32ec4949
KarolWojtas <karol.wojtas.aim@gmail.com>
2018-08-28 20:07:46 +0200

    initial

---
+++ src/test/java/com/karol/ConhubUserServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.karol;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConhubUserServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
